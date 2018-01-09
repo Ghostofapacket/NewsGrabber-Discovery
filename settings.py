b996e2728f7388e61f9253be17c4cd715140e55b
Iggersm00 <matt@iggo.co.uk>
2018-01-09 20:13:01 +0000

    Update irc server settings
    Update irc from underworld.no which is continually netsplitting / disconnecting clients to irc.efnet.org to allow splitting of services across multiple servers using efnets load balancing method.

--- settings.py
+++ settings.py
@@ -26,7 +26,7 @@ def init():
     irc_channel_bot = '#newsgrabberbot'
     irc_channel_main = '#newsgrabber'
     irc_nick = 'd_' + str(random.randint(0, 5000))
-    irc_server_name = 'irc.underworld.no'
+    irc_server_name = 'irc.efnet.org'
     irc_server_port = 6667
     dir_assigned_services = 'assigned_services'
     log_file_name = 'log.log'
